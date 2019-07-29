669703f50cca17053413491d3710bd7eab172b63
José Francisco González <46899536+JoseFranciscoGonzalez@users.noreply.github.com>
2019-07-29 14:55:04 -0300

    Agrega funcion iterativa y pruebas

--- CORDIC.py
+++ CORDIC.py
@@ -17,7 +17,7 @@ import math
 
 def rotation_mode(x_i, y_i, i, angle_accumulator):
     
-    """ Iteración de rotación de un vector en un ángulo 
+    """ Iteración de rotación de un vector en un ángulo en radianes 
     
     Parámetros
     ----------
@@ -43,23 +43,100 @@ def rotation_mode(x_i, y_i, i, angle_accumulator):
         Posición final del vector en y
         
     angle_accumulator : float    
-        Nuevo resto angular
+        Nuevo resto angular en radianes
         
     """
-    
-    if angle_accumulator < 0:
+    if angle_accumulator > 0:
         direction_i = 1
     else:
         direction_i = -1
     
     x = x_i - y_i*direction_i*(2**(-i))
-    y = y_i + x_i*direction_i*(2**(-i))
-    
+    y = y_i + x_i*direction_i*(2**(-i)) 
+
     angle_accumulator = angle_accumulator - direction_i*math.atan(2**(-i))
     
     return x, y, angle_accumulator
 
 
-x, y, angle = rotation_mode(0.939, 0.342, 0 , 20)
-print(x,y,angle)
+def multiple_rotations(initial_angle, rotation_angle, iterations):
+	""" Múltiples corrimientos de corrección al ángulo de rotación
+
+	Parámetros
+	----------
+	initial_angle : float
+		Ángulo inicial del vector en radianes.
+
+	rotation_angle : float
+		Ángulo a rotar en radianes.
+
+	iterations : int
+		Cantidad de iteraciones del algoritmo.
+
+	Retornos
+	--------
+	x : float
+		Posición final en x
+
+	y : float
+		Posición final en y
+
+	angle_accumulator : float
+		Resto del ángulo del corrimiento
+
+	"""
+
+	i = 0
+
+	x = math.cos(initial_angle)
+	y = math.sin(initial_angle)
+
+	angle_accumulator = rotation_angle
+
+	while i < iterations:
+		x, y, angle_accumulator = rotation_mode(x, y, i, angle_accumulator)
+		i += 1
+
+	return x, y, angle_accumulator
+
+
+def preprocess(initial_angle,rotation_angle, iterations):
+	""" Ajustes del ángulo inicial
+
+	Parámetros
+	----------
+	initial_angle : float
+		Ángulo inicial del vector en radianes.
+
+	rotation_angle : float
+		Ángulo a rotar en radianes.
+
+	iterations : int
+		Cantidad de iteraciones del algoritmo.
+
+	"""
+
+	#if initial_angle < 180
+
+
+# =============================================================================
+# PRUEBAS
+# =============================================================================
+
+
+initial_angle = (-90*math.pi)/180
+rotation_angle = (-100*math.pi)/180
+
+x, y, angle_accumulator = multiple_rotations(initial_angle, rotation_angle, 30)
+
+if x < 0 and y > 0:
+	angle = 180 + (math.atan(y/x)*180)/math.pi
+
+elif x < 0 and y < 0:
+	angle = -180 + (math.atan(y/x)*180)/math.pi
+
+else :
+	angle = (math.atan(y/x)*180)/math.pi
+
+print(angle, angle_accumulator)
 
